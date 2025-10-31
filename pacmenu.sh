afd4d2df634521220e744211336a485212d5e8d0
Felipe Duarte <felipesdrs@hotmail.com>
2025-10-31 14:01:42 -0300

    feat: added async input for dynamically loading list

--- pacmenu.sh
+++ pacmenu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
-# TODO: The reload in line 176 is quite memory intensive, look into alternatives.
+# TODO: issuing a reload() while streaming in input
+# causes a crash, debug
 
 trap 'exit_handler' INT TERM
 trap 'cleanup' EXIT
@@ -37,7 +38,8 @@ function exit_handler() {
 function cleanup() {
     rm -f "${FILES[@]}" 2>/dev/null
 
-    pkill -P $$ 2>/dev/null || true
+    pkill -P "${FZF_PID}" 2>/dev/null || true
+    pkill -P "${$}" 2>/dev/null || true
     wait 2>/dev/null || true
 }
 
@@ -53,20 +55,26 @@ function check_depends() {
 function populate_lists() {
     declare REPO PACKAGE VERSION INSTALLED || exit 1
     declare -a TARGETS FORMATS || exit 1
+    exec 3>"${FILES[pipe]}" || exit 1
 
     while read -r REPO PACKAGE VERSION INSTALLED; do
         FORMATS[1]="${COLORS[gray]}${REPO} ${COLORS[white]}${PACKAGE} ${COLORS[gray]}${VERSION}"
         FORMATS[0]="${FORMATS[1]} ${INSTALLED}"
 
-        [[ "${REPO}" == "aur" ]] && TARGETS+=("${FILES["${REPO}"]}") || TARGETS+=("${FILES[repos]}")
-        [[ "${INSTALLED}" == "[installed]" ]] && TARGETS+=("${FILES[uninstall]}")
+        [[ "${REPO}" == "aur" ]] && TARGETS[0]="${FILES["${REPO}"]}" || TARGETS[0]="${FILES[repos]}"
+        [[ "${INSTALLED}" == "[installed]" ]] && TARGETS[1]="${FILES[uninstall]}" || TARGETS[1]="/dev/null"
 
         for i in "${!TARGETS[@]}"; do
+            [[ "${TARGETS[i]}" == "${FILES["${START_MODE}"]}" ]] && FORMATS[2]="${FORMATS[i]}"
             printf "%b\n" "${FORMATS[i]}" >> "${TARGETS[i]}"
         done
 
+        printf "%b\n" "${FORMATS[2]}" >&3
+
         TARGETS=()
     done
+
+    exec 3>&-
 }
 
 declare -A FILES=(
@@ -74,6 +82,8 @@ declare -A FILES=(
     [repos]="/tmp/pac_repos.txt"
     [aur]="/tmp/pac_aur.txt"
     [mode]="/tmp/pac_mode.txt"
+    [pipe]="/tmp/pac_pipe"
+    [selection]="/tmp/pac_selection.txt"
 ) || exit 1
 
 declare -A COLORS=(
@@ -86,32 +96,40 @@ declare -a DEPENDS=(fzf) || exit 1
 declare -a FZF_ARGS=(
     --no-mouse
     --multi
-    --sync
     --ansi
     --reverse
+    --prompt=": "
     --info="inline-right"
     --border="rounded"
     --border-label="⎸ pacman ⎹"
-    --prompt=": "
     --input-border="rounded"
     --input-label=""
     --list-border="rounded"
     --list-label=""
     --preview=""
-    --preview-label=""
     --preview-border="rounded"
+    --preview-label=""
+    --preview-window="right,60%"
     --footer-label="⎸ Selected packages ⎹"
     --footer-border="rounded"
     --bind="change:first"
-    --bind="start:transform-preview-label(
+    --bind="start:bg-transform-preview-label(
                 echo \"⎸ {2} ⎹\"
-            )+transform-input-label(
+            )+bg-transform-input-label(
                 MODE=\$(<\"/tmp/pac_mode.txt\")
                 case \$MODE in
                     repos) echo \"⎸ [core] and [extra] ⎹\" ;;
                     aur) echo \"⎸ AUR ⎹\" ;;
                     uninstall) echo \"⎸ Uninstall ⎹\" ;;
                 esac
+            )"
+    --bind="load:bg-transform-preview-label(
+                echo \"⎸ {2} ⎹\"
+            )+preview(
+                MODE=\$(<\"/tmp/pac_mode.txt\")
+                [[ \$MODE == \"uninstall\" ]] && \\
+                    \$PKG_MANAGER -Qi {2} || \\
+                    \$PKG_MANAGER -Si {2}
             )"
     --bind="ctrl-s:transform-input-label(
                 MODE=\$(<\"/tmp/pac_mode.txt\")
@@ -145,20 +163,19 @@ declare -a FZF_ARGS=(
                     uninstall) cat /tmp/pac_uninstall.txt ;;
                 esac
             )"
-    --bind="result:transform-list-label(
+    --bind="result:bg-transform-list-label(
                 [[ -n \$FZF_QUERY ]] && \\
                     echo \"⎸ \$FZF_MATCH_COUNT matches for '\$FZF_QUERY' ⎹\" || \\
                     echo \"\"
             )"
-    --bind="multi:transform-footer(
+    --bind="multi:bg-transform-footer(
                 if [[ \$FZF_SELECT_COUNT -ge 1 ]]; then
                     MODE=\$(<\"/tmp/pac_mode.txt\")
-                    [[ \$MODE == \"uninstall\" ]] && \\
-                        COLOR=\$(tput setaf 1)
+                    [[ \$MODE == \"uninstall\" ]] && COLOR=\$(tput setaf 1)
                     printf \"\$COLOR%s\n\" {+}
                 fi
             )"
-    --bind="load:transform-preview-label(
+    --bind="focus:bg-transform-preview-label(
                 echo \"⎸ {2} ⎹\"
             )+preview(
                 MODE=\$(<\"/tmp/pac_mode.txt\")
@@ -166,30 +183,15 @@ declare -a FZF_ARGS=(
                     \$PKG_MANAGER -Qi {2} || \\
                     \$PKG_MANAGER -Si {2}
             )"
-    --bind="focus:transform-preview-label(
-                echo \"⎸ {2} ⎹\"
-            )+preview(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                [[ \$MODE == \"uninstall\" ]] && \\
-                    \$PKG_MANAGER -Qi {2} || \\
-                    \$PKG_MANAGER -Si {2}
-            )+reload(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                case \$MODE in
-                    repos) cat /tmp/pac_repos.txt ;;
-                    aur) cat /tmp/pac_aur.txt ;;
-                    uninstall) cat /tmp/pac_uninstall.txt ;;
-                esac
-            )"
-    --color="info:#4C4F69,spinner:#4C4F69,border:#FAB387,label:#FAB387,
-            prompt:#89B4FA,hl:#A6E3A1,hl+:#A6E3A1,input-border:#A6E3A1,
-            input-label:#A6E3A1,footer-border:#4C4F69,footer-label:#4C4F69,
-            footer-fg:#89B4FA,list-border:#CDD6F4,list-label:#CDD6F4,
-            marker:#A6E3A1,preview-border:#89B4FA,preview-label:#89B4FA"
+    --color="prompt:#89B4FA,info:#4C4F69,spinner:#4C4F69,border:#FAB387,marker:#A6E3A1,
+            label:#FAB387,input-label:#A6E3A1,list-label:#CDD6F4,preview-label:#89B4FA,footer-label:#4C4F69,
+            input-border:#A6E3A1,list-border:#CDD6F4,preview-border:#89B4FA, footer-border:#4C4F69,
+            hl:#A6E3A1,hl+:#A6E3A1,footer-fg:#89B4FA"
 ) || exit 1
 
 declare PKG_MANAGER="pacman" || exit 1
 declare START_MODE="repos" || exit 1
+declare FZF_PID
 
 function main() {
     declare -a SELECTION PACKAGES
@@ -219,7 +221,7 @@ function main() {
                             usage >&2
                             exit 1
                         fi
-                        ;&
+                        ;;
 
                     repos|uninstall)
                         START_MODE="${OPTARG}"
@@ -246,13 +248,18 @@ function main() {
     done
 
     check_depends
-    populate_lists < <(${PKG_MANAGER} -Sl) &
 
     export PKG_MANAGER
-    echo "${START_MODE}" > "${FILES[mode]}" &
-    while [[ ! -f "${FILES["${START_MODE}"]}" ]]; do true; done
+    mkfifo "${FILES[pipe]}"
+    echo "${START_MODE}" > "${FILES[mode]}"
 
-    mapfile -t SELECTION < <(fzf "${FZF_ARGS[@]}" < "${FILES["${START_MODE}"]}") || exit 1
+    fzf "${FZF_ARGS[@]}" < "${FILES[pipe]}" > "${FILES[selection]}" &
+    FZF_PID="${!}"
+
+    populate_lists < <("${PKG_MANAGER}" -Sl)
+    wait "${FZF_PID}" || true
+
+    mapfile -t SELECTION < "${FILES[selection]}"
     [[ -z "${SELECTION[*]}" ]] && echo "No packages selected." && exit 0
 
     PACKAGES=("${SELECTION[@]#* }")
