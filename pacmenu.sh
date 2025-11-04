dd7193b0d980cc35f086cc22f1ccd7c4ef55f823
Felipe Duarte <felipesdrs@hotmail.com>
2025-11-04 19:01:28 -0300

    refactor!: read extended description
    - Added privilege escalation support for package manipulation
    - Changed tempfile directory to /run/user/$UID/pac
    - Restricted scope of all variables to the minimum
    - Made most menu-dependent labels update on start() as opposed to being
      hardcoded
    - General reordering of definitions (variables, functions, main)
    - General cleaning of unnecessary exit calls

--- pacmenu.sh
+++ pacmenu.sh
@@ -3,6 +3,124 @@
 trap 'exit_handler' INT TERM
 trap 'cleanup' EXIT
 
+declare REINSTALL WRITER_PID
+declare TMP_DIR="${XDG_RUNTIME_DIR:-/run/user/${UID}}/pac" || exit 1
+
+declare -A FILES=(
+    [uninstall]="${TMP_DIR}/uninstall.txt"
+    [repos]="${TMP_DIR}/repos.txt"
+    [aur]="${TMP_DIR}/aur.txt"
+    [mode]="${TMP_DIR}/mode.txt"
+) || exit 1
+
+declare -A ANSI=(
+    [gray]="$(tput setaf 0)"
+    [bold]="$(tput bold)"
+    [italic]="$(tput sitm)"
+    [underline]="$(tput smul)"
+    [reset]="$(tput sgr0)"
+) || exit 1
+
+declare -a DEPENDS=(fzf tail tput pkill) || exit 1
+declare -a FZF_ARGS=(
+    --multi
+    --ansi
+    --reverse
+    --info="inline-right"
+    --border="rounded"
+    --border-label="⎸ pacman ⎹"
+    --input-border="rounded"
+    --list-border="rounded"
+    --preview-border="rounded"
+    --preview-window="right,60%"
+    --footer-label="⎸ selected packages ⎹"
+    --footer-border="rounded"
+    --bind="change:first"
+    --bind="start:bg-transform-prompt(
+                echo \"loading : \"
+            )+bg-transform-preview-label(
+                echo \"⎸ {2} ⎹\"
+            )+bg-transform-input-label(
+                case \$START_MODE in
+                    repos) echo \"⎸ [core] and [extra] ⎹\" ;;
+                    aur) echo \"⎸ aur ⎹\" ;;
+                    uninstall) echo \"⎸ uninstall ⎹\" ;;
+                esac
+            )+reload(
+                case \$START_MODE in
+                    repos) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/repos.txt ;;
+                    aur) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/aur.txt ;;
+                    uninstall) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/uninstall.txt ;;
+                esac
+            )"
+    --bind="load:bg-transform-preview-label(
+                echo \"⎸ {2} ⎹\"
+            )+bg-transform-prompt(
+                echo \": \"
+            )+preview(
+                MODE=\$(<\"\$TMP_DIR/mode.txt\")
+                [[ \$MODE == \"uninstall\" ]] && \\
+                    \$PKG_MANAGER -Qi {2} || \\
+                    \$PKG_MANAGER -Si {2}
+            )"
+    --bind="ctrl-s:transform-input-label(
+                MODE=\$(<\"\$TMP_DIR/mode.txt\")
+                case \$MODE in
+                    repos)
+                        if [[ \$PKG_MANAGER != \"pacman\" ]]; then
+                            MODE=\"aur\"
+                            echo \"⎸ aur ⎹\"
+                        else
+                            MODE=\"uninstall\"
+                            echo \"⎸ uninstall ⎹\"
+                        fi
+                    ;;
+
+                    aur)
+                        MODE=\"uninstall\"
+                        echo \"⎸ uninstall ⎹\"
+                    ;;
+
+                    uninstall)
+                        MODE=\"repos\"
+                        echo \"⎸ [core] and [extra] ⎹\"
+                    ;;
+                esac
+                echo \$MODE > \$TMP_DIR/mode.txt
+            )+reload(
+                MODE=\$(<\"\$TMP_DIR/mode.txt\")
+                case \$MODE in
+                    repos) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/repos.txt ;;
+                    aur) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/aur.txt ;;
+                    uninstall) tail -F --lines=+0 --pid=\$WRITER_PID \$TMP_DIR/uninstall.txt ;;
+                esac
+            )"
+    --bind="result:bg-transform-list-label(
+                [[ -n \$FZF_QUERY ]] && \\
+                    echo \"⎸ \$FZF_MATCH_COUNT matches for '\$FZF_QUERY' ⎹\" || \\
+                    echo \"\"
+            )"
+    --bind="multi:bg-transform-footer(
+                if [[ \$FZF_SELECT_COUNT -ge 1 ]]; then
+                    MODE=\$(<\"\$TMP_DIR/mode.txt\")
+                    [[ \$MODE == \"uninstall\" ]] && COLOR=\$(tput setaf 1)
+                    printf \"\$COLOR%s\n\" {+}
+                fi
+            )"
+    --bind="focus:bg-transform-preview-label(
+                echo \"⎸ {2} ⎹\"
+            )+preview(
+                MODE=\$(<\"\$TMP_DIR/mode.txt\")
+                [[ \$MODE == \"uninstall\" ]] && \\
+                    \$PKG_MANAGER -Qi {2} || \\
+                    \$PKG_MANAGER -Si {2}
+            )"
+    --color="prompt:#89B4FA,info:#4C4F69,spinner:#4C4F69,border:#FAB387,marker:#A6E3A1,
+            label:#FAB387,input-label:#A6E3A1,list-label:#CDD6F4,preview-label:#89B4FA,footer-label:#4C4F69,
+            input-border:#A6E3A1,list-border:#CDD6F4,preview-border:#89B4FA,footer-border:#4C4F69,
+            hl:#A6E3A1,hl+:#A6E3A1,footer-fg:#89B4FA"
+) || exit 1
+
 function usage() {
 echo "pacmenu - An opinionated fzf-powered menu for Pacman
 
@@ -12,10 +130,14 @@ echo "pacmenu - An opinionated fzf-powered menu for Pacman
     ${ANSI[bold]}Usage: pacmenu.sh${ANSI[reset]} [OPTIONS]
 
     ${ANSI[bold]}Options:${ANSI[reset]}
+
+        ${ANSI[bold]}-a, --auth${ANSI[reset]} [${ANSI[underline]}run0${ANSI[reset]}|sudo|doas]
+                choose which privilege escalation program to run when running the operation.
+
         ${ANSI[bold]}-p, --package-manager${ANSI[reset]} [paru|yay]
                 select alternative package manager to use, enabling the aur menu if applicable.
 
-        ${ANSI[bold]}-s, --start-mode${ANSI[reset]} [repos|aur|uninstall]
+        ${ANSI[bold]}-s, --start-mode${ANSI[reset]} [${ANSI[underline]}repos${ANSI[reset]}|aur|uninstall]
                 allows starting from any of the three available menus.
 
         ${ANSI[bold]}-r, --reinstall${ANSI[reset]}
@@ -38,33 +160,35 @@ function exit_handler() {
 
 function cleanup() {
     rm -f "${FILES[@]}" 2>/dev/null
+    rmdir "${TMP_DIR}" 2>/dev/null
 
     pkill -P "${$}" 2>/dev/null || true
     wait 2>/dev/null || true
 }
 
 function arg_error() {
+    declare PROGRAM_NAME="pacmenu"
     declare TYPE="${1}"
     declare OPTION="${2}"
     declare ARGUMENT="${3}"
-    declare COMMENT="${4}"
 
     case "${TYPE}" in
         option)
-            printf "%s: invalid option -- '%s'\n%s\n" \
+            printf "%s: invalid option -- '%s'\n" \
                 "${PROGRAM_NAME}" \
-                "${OPTION}" \
-                "${COMMENT}" ;;
+                "${OPTION}" ;;
 
         argument)
-            printf "%s: invalid argument for '%s' -- '%s'\n%s\n" \
+            printf "%s: invalid argument for '%s' -- '%s'\n" \
                 "${PROGRAM_NAME}" \
                 "${OPTION}" \
-                "${ARGUMENT}" \
-                "${COMMENT}" ;;
+                "${ARGUMENT}" ;;
 
         *) exit 1 ;;
     esac
+
+    usage >&2
+    exit 1
 }
 
 function check_depends() {
@@ -77,7 +201,7 @@ function check_depends() {
 }
 
 function populate_lists() {
-    declare REPO PACKAGE VERSION INSTALLED || exit 1
+    declare REPO PACKAGE VERSION INSTALLED
     declare -a TARGETS FORMATS || exit 1
 
     while read -r REPO PACKAGE VERSION INSTALLED; do
@@ -94,135 +218,29 @@ function populate_lists() {
     done
 }
 
-declare PKG_MANAGER="pacman" || exit 1
-declare START_MODE="repos" || exit 1
-declare PROGRAM_NAME="pacmenu" || exit 1
-declare REINSTALL WRITER_PID || exit 1
-
-declare -A FILES=(
-    [uninstall]="/tmp/pac_uninstall.txt"
-    [repos]="/tmp/pac_repos.txt"
-    [aur]="/tmp/pac_aur.txt"
-    [mode]="/tmp/pac_mode.txt"
-) || exit 1
-
-declare -A ANSI=(
-    [gray]="$(tput setaf 0)"
-    [bold]="$(tput bold)"
-    [italic]="$(tput sitm)"
-    [reset]="$(tput sgr0)"
-) || exit 1
-
-declare -a PACKAGES || exit 1
-declare -a DEPENDS=(fzf tail tput pkill) || exit 1
-declare -a FZF_ARGS=(
-    --no-mouse
-    --multi
-    --ansi
-    --reverse
-    --prompt="loading : "
-    --info="inline-right"
-    --border="rounded"
-    --border-label="⎸ pacman ⎹"
-    --input-border="rounded"
-    --input-label=""
-    --list-border="rounded"
-    --list-label=""
-    --preview=""
-    --preview-border="rounded"
-    --preview-label=""
-    --preview-window="right,60%"
-    --footer-label="⎸ Selected packages ⎹"
-    --footer-border="rounded"
-    --bind="change:first"
-    --bind="start:bg-transform-preview-label(
-                echo \"⎸ {2} ⎹\"
-            )+bg-transform-input-label(
-                case \$START_MODE in
-                    repos) echo \"⎸ [core] and [extra] ⎹\" ;;
-                    aur) echo \"⎸ aur ⎹\" ;;
-                    uninstall) echo \"⎸ uninstall ⎹\" ;;
-                esac
-            )+reload(
-                case \$START_MODE in
-                    repos) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_repos.txt ;;
-                    aur) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_aur.txt ;;
-                    uninstall) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_uninstall.txt ;;
-                esac
-            )"
-    --bind="load:bg-transform-preview-label(
-                echo \"⎸ {2} ⎹\"
-            )+bg-transform-prompt(
-                echo \":\"
-            )+preview(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                [[ \$MODE == \"uninstall\" ]] && \\
-                    \$PKG_MANAGER -Qi {2} || \\
-                    \$PKG_MANAGER -Si {2}
-            )"
-    --bind="ctrl-s:transform-input-label(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                case \$MODE in
-                    repos)
-                        if [[ \$PKG_MANAGER != \"pacman\" ]]; then
-                            MODE=\"aur\"
-                            echo \"⎸ aur ⎹\"
-                        else
-                            MODE=\"uninstall\"
-                            echo \"⎸ uninstall ⎹\"
-                        fi
-                    ;;
-
-                    aur)
-                        MODE=\"uninstall\"
-                        echo \"⎸ uninstall ⎹\"
-                    ;;
-
-                    uninstall)
-                        MODE=\"repos\"
-                        echo \"⎸ [core] and [extra] ⎹\"
-                    ;;
-                esac
-                echo \$MODE > /tmp/pac_mode.txt
-            )+reload(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                case \$MODE in
-                    repos) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_repos.txt ;;
-                    aur) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_aur.txt ;;
-                    uninstall) tail -F --lines=+0 --pid=\$WRITER_PID /tmp/pac_uninstall.txt ;;
-                esac
-            )"
-    --bind="result:bg-transform-list-label(
-                [[ -n \$FZF_QUERY ]] && \\
-                    echo \"⎸ \$FZF_MATCH_COUNT matches for '\$FZF_QUERY' ⎹\" || \\
-                    echo \"\"
-            )"
-    --bind="multi:bg-transform-footer(
-                if [[ \$FZF_SELECT_COUNT -ge 1 ]]; then
-                    MODE=\$(<\"/tmp/pac_mode.txt\")
-                    [[ \$MODE == \"uninstall\" ]] && COLOR=\$(tput setaf 1)
-                    printf \"\$COLOR%s\n\" {+}
-                fi
-            )"
-    --bind="focus:bg-transform-preview-label(
-                echo \"⎸ {2} ⎹\"
-            )+preview(
-                MODE=\$(<\"/tmp/pac_mode.txt\")
-                [[ \$MODE == \"uninstall\" ]] && \\
-                    \$PKG_MANAGER -Qi {2} || \\
-                    \$PKG_MANAGER -Si {2}
-            )"
-    --color="prompt:#89B4FA,info:#4C4F69,spinner:#4C4F69,border:#FAB387,marker:#A6E3A1,
-            label:#FAB387,input-label:#A6E3A1,list-label:#CDD6F4,preview-label:#89B4FA,footer-label:#4C4F69,
-            input-border:#A6E3A1,list-border:#CDD6F4,preview-border:#89B4FA,footer-border:#4C4F69,
-            hl:#A6E3A1,hl+:#A6E3A1,footer-fg:#89B4FA"
-) || exit 1
-
 function main() {
-    declare -a SELECTION PACKAGES
+    declare START_MODE="repos"
+    declare PKG_MANAGER="pacman"
+    declare AUTH="run0"
+    declare PROCESS
+    declare -a SELECTION PACKAGES || exit 1
+    declare -A FLAGS=(
+        [install]="-S"
+        [uninstall]="-Rns"
+    ) || exit 1
 
     while [[ "${#}" -gt 0 ]]; do
         case "${1}" in
+            -a|--auth)
+                case "${2}" in
+                    run0|sudo|doas)
+                        AUTH="${2}"
+                        DEPENDS+=("${AUTH}")
+                        shift 2 ;;
+
+                    *) arg_error "argument" "${1}" "${2}" ;;
+                esac ;;
+
             -p|--package-manager)
                 case "${2}" in
                     paru|yay)
@@ -230,29 +248,18 @@ function main() {
                         DEPENDS+=("${PKG_MANAGER}")
                         shift 2 ;;
 
-                    *)
-                        arg_error "argument" "${1}" "${2}"
-                        usage >&2
-                        exit 1 ;;
+                    *) arg_error "argument" "${1}" "${2}" ;;
                 esac ;;
 
             -s|--start-mode)
                 case "${2}" in
-                    aur)
-                        if [[ "${PKG_MANAGER}" == "pacman" ]]; then
-                            arg_error "argument" "${1}" "${2}" "(choose an aur-compatible package manager for aur mode)\n"
-                            usage >&2
-                            exit 1
-                        fi ;&
+                    aur) [[ "${PKG_MANAGER}" == "pacman" ]] && arg_error "argument" "${1}" "${2}" ;&
 
                     repos|uninstall)
                         START_MODE="${2}"
                         shift 2 ;;
 
-                    *)
-                        arg_error "argument" "${1}" "${2}"
-                        usage >&2
-                        exit 1 ;;
+                    *) arg_error "argument" "${1}" "${2}" ;;
                 esac ;;
 
             -r|--reinstall)
@@ -263,16 +270,14 @@ function main() {
                 usage
                 exit 0 ;;
 
-            *)
-                arg_error "option" "${1}" "${2}"
-                usage >&2
-                exit 1 ;;
+            *) arg_error "option" "${1}" "${2}" ;;
         esac
     done
 
     check_depends
 
-    export PKG_MANAGER START_MODE
+    export PKG_MANAGER START_MODE TMP_DIR
+    [[ -d "${TMP_DIR}" ]] || mkdir "${TMP_DIR}"
     echo "${START_MODE}" > "${FILES[mode]}"
 
     populate_lists < <("${PKG_MANAGER}" -Sl) &
@@ -281,26 +286,23 @@ function main() {
 
     while [[ ! -f "${FILES["${START_MODE}"]}" ]]; do true; done
     mapfile -t SELECTION < <(fzf "${FZF_ARGS[@]}")
-    kill -9 "${WRITER_PID}" 2>/dev/null || true
 
     [[ -z "${SELECTION[*]}" ]] && echo "No packages selected." && exit 0
     PACKAGES=("${SELECTION[@]#* }")
     PACKAGES=("${PACKAGES[@]%% *}")
 
     case "$(<"${FILES[mode]}")" in
-        repos|aur)
-            "${PKG_MANAGER}" -S "${PACKAGES[@]}"
-            ;;
-
-        uninstall)
-            "${PKG_MANAGER}" -Rns "${PACKAGES[@]}"
-            ;;
+        uninstall) PROCESS="uninstall" ;;
+        aur) unset AUTH ;&
+        repos) PROCESS="install" ;;
 
         *)
-            echo "ERROR: Unknown mode ${MODE}."
+            printf "pacmenu: Unknown mode -- '%s'\n" "${MODE}"
             exit 1
             ;;
     esac
+
+    "${AUTH}" "${PKG_MANAGER}" "${FLAGS["${PROCESS}"]}" "${PACKAGES[@]}"
 }
 
 main "${@}"
